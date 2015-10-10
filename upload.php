e077774f08a50fe7a6e91f056be1639572fe9531
iCodeart <servicios@icodeart.com>
2015-10-10 17:54:31 -0200

    Update upload.php

--- upload.php
+++ upload.php
@@ -1,5 +1,24 @@
 <?php
 
+/**
+**
+**  BY iCODEART
+**
+**********************************************************************
+**                      REDES SOCIALES                            ****
+**********************************************************************
+**                                                                ****
+** FACEBOOK: https://www.facebook.com/icodeart                    ****
+** TWIITER: https://twitter.com/icodeart                          ****
+** YOUTUBE: https://www.youtube.com/c/icodeartdeveloper           ****
+** GITHUB: https://github.com/icodeart                            ****
+** TELEGRAM: https://telegram.me/icodeart                         ****
+** EMAIL: info@icodeart.com                                       ****
+**                                                                ****
+**********************************************************************
+**********************************************************************
+**/
+
 // Verificamos si se ha enviado el form
 if (isset($_POST["subir"]))
 {
